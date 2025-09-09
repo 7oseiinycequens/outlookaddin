d00fb74313f2f990efadde1953257cddca81a7eb
elhoseiny <a.elhoseiny@cequens.com>
2025-09-09 14:26:25 +0300

    1

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 const path = require('path');
 
-app.get('/outlook/web/index.html', (req, res) => {
-	res.sendFile(path.join(__dirname, 'outlook', 'web', 'index.html'));
+app.get('/web/index.html', (req, res) => {
+	res.sendFile(path.join(__dirname, 'web', 'index.html'));
 });
 
 app.get('/', (req, res) => {
